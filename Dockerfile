f6b6ffb3dc06f15acdd85fce50b3fb415dbbfc98
danielchu <jiahao.chu.dc@gmail.com>
2020-03-19 08:36:49 +0800

    new update
    https://github.com/chemidy/smallest-secured-golang-docker-image

--- Dockerfile
+++ Dockerfile
@@ -2,23 +2,70 @@
 # Start from a Debian image with the latest version of Go installed
 # and a workspace (GOPATH) configured at /go.
 # FROM s390x/golang:1.14-alpine
-FROM golang:14
 
-# Copy the local package files to the container's workspace.
-ADD . /go/src/github.com/golang/example/outyet
+############################
+# STEP 1 build executable binary
+############################
+# golang alpine 1.13.5
+FROM golang@sha256:0991060a1447cf648bab7f6bb60335d1243930e38420bee8fec3db1267b84cfa as builder
+
+
+# Create appuser
+ENV USER=appuser
+ENV UID=10001
+
+# Install git + SSL ca certificates.
+# Git is required for fetching the dependencies.
+# Ca-certificates is required to call HTTPS endpoints.
+RUN apk update && apk add --no-cache git ca-certificates tzdata && update-ca-certificates
+
+
+# See https://stackoverflow.com/a/55757473/12429735
+RUN adduser \
+   --disabled-password \
+   --gecos "" \
+   --home "/nonexistent" \
+   --shell "/sbin/nologin" \
+   --no-create-home \
+   --uid "${UID}" \
+   "${USER}"
+WORKDIR $GOPATH/src/mypackage/myapp/
+COPY . .
 
 # Fix the error cannot find package "golang.org/x/net/html" in any of:
 # default: #10 0.286 	/usr/local/go/src/golang.org/x/net/html (from $GOROOT)
 # default: #10 0.286 	/go/src/golang.org/x/net/html (from $GOPATH)
-RUN go get -d -v golang.org/x/net/html 
+# RUN go get -d -v golang.org/x/net/html 
+# Fetch dependencies.
+RUN go get -d -v
 
-# Build the outyet command inside the container.
-# (You may fetch or manage dependencies here,
-# either manually or with a tool like "godep".)
-RUN go install github.com/golang/example/outyet
+# Build the binary
+RUN CGO_ENABLED=0 GOOS=linux go build \
+   -ldflags='-w -s -extldflags "-static"' -a \
+   -o /go/bin/hello .
+
+############################
+# STEP 2 build a small image
+############################
+FROM scratch
+
+# Import from builder.
+COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /etc/group /etc/group
+
+# Copy our static executable
+COPY --from=builder /go/bin/hello /go/bin/hello
+
+# Use an unprivileged user.
+USER appuser:appuser
+
+# Run the hello binary.
+ENTRYPOINT ["/go/bin/hello"]
 
 # Run the outyet command by default when the container starts.
-ENTRYPOINT /go/bin/outyet
+# ENTRYPOINT /go/bin/outyet
 
 # Document that the service listens on port 8080.
 EXPOSE 8080
--- app.go
+++ app.go
@@ -1,93 +1,93 @@
-package main
+// package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"strings"
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"net/url"
+// 	"os"
+// 	"strings"
 
-	"golang.org/x/net/html"
-)
+// 	"golang.org/x/net/html"
+// )
 
-type scrapeDataStore struct {
-	Internal int `json:"internal"`
-	External int `json:"external"`
-}
+// type scrapeDataStore struct {
+// 	Internal int `json:"internal"`
+// 	External int `json:"external"`
+// }
 
-func isInternal(parsedLink *url.URL, siteURL *url.URL, link string) bool {
-	return parsedLink.Host == siteURL.Host || strings.Index(link, "#") == 0 || len(parsedLink.Host) == 0
-}
+// func isInternal(parsedLink *url.URL, siteURL *url.URL, link string) bool {
+// 	return parsedLink.Host == siteURL.Host || strings.Index(link, "#") == 0 || len(parsedLink.Host) == 0
+// }
 
-func main() {
-	urlIn := os.Getenv("url")
-	if len(urlIn) == 0 {
-		urlIn = "https://www.alexellis.io/"
-		// log.Fatalln("Need a valid url as an env-var.")
-	}
+// func main() {
+// 	urlIn := os.Getenv("url")
+// 	if len(urlIn) == 0 {
+// 		urlIn = "https://www.alexellis.io/"
+// 		// log.Fatalln("Need a valid url as an env-var.")
+// 	}
 
-	siteURL, parseErr := url.Parse(urlIn)
+// 	siteURL, parseErr := url.Parse(urlIn)
 
-	if parseErr != nil {
-		log.Fatalln(parseErr)
-	}
+// 	if parseErr != nil {
+// 		log.Fatalln(parseErr)
+// 	}
 
-	resp, err := http.Get(urlIn)
-	if err != nil {
-		log.Fatalln(err)
-	}
+// 	resp, err := http.Get(urlIn)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
 
-	scrapeData := &scrapeDataStore{}
+// 	scrapeData := &scrapeDataStore{}
 
-	tokenizer := html.NewTokenizer(resp.Body)
-	end := false
-	for {
-		tt := tokenizer.Next()
-		switch {
-		case tt == html.StartTagToken:
-			// fmt.Println(tt)
-			token := tokenizer.Token()
-			switch token.Data {
-			case "a":
+// 	tokenizer := html.NewTokenizer(resp.Body)
+// 	end := false
+// 	for {
+// 		tt := tokenizer.Next()
+// 		switch {
+// 		case tt == html.StartTagToken:
+// 			// fmt.Println(tt)
+// 			token := tokenizer.Token()
+// 			switch token.Data {
+// 			case "a":
 
-				for _, attr := range token.Attr {
+// 				for _, attr := range token.Attr {
 
-					if attr.Key == "href" {
-						link := attr.Val
+// 					if attr.Key == "href" {
+// 						link := attr.Val
 
-						parsedLink, parseLinkErr := url.Parse(link)
+// 						parsedLink, parseLinkErr := url.Parse(link)
 
-						// debug
-						// if strings.Index(link, "#") == 0 {
-						// 	fmt.Println(link)
-						// } else {
-						// 	fmt.Println(parsedLink)
-						// }
-						if parseLinkErr == nil {
-							if isInternal(parsedLink, siteURL, link) {
-								scrapeData.Internal++
-							} else {
-								scrapeData.External++
-							}
-						}
+// 						// debug
+// 						// if strings.Index(link, "#") == 0 {
+// 						// 	fmt.Println(link)
+// 						// } else {
+// 						// 	fmt.Println(parsedLink)
+// 						// }
+// 						if parseLinkErr == nil {
+// 							if isInternal(parsedLink, siteURL, link) {
+// 								scrapeData.Internal++
+// 							} else {
+// 								scrapeData.External++
+// 							}
+// 						}
 
-						if parseLinkErr != nil {
-							fmt.Println("Can't parse: " + token.Data)
-						}
-					}
-				}
-				break
-			}
-		case tt == html.ErrorToken:
-			end = true
-			break
-		}
-		if end {
-			break
-		}
-	}
-	data, _ := json.Marshal(&scrapeData)
-	fmt.Println(string(data))
-}
+// 						if parseLinkErr != nil {
+// 							fmt.Println("Can't parse: " + token.Data)
+// 						}
+// 					}
+// 				}
+// 				break
+// 			}
+// 		case tt == html.ErrorToken:
+// 			end = true
+// 			break
+// 		}
+// 		if end {
+// 			break
+// 		}
+// 	}
+// 	data, _ := json.Marshal(&scrapeData)
+// 	fmt.Println(string(data))
+// }
